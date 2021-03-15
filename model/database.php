2bfb8b7390300e05dd3fee9a509eb13f27a67ddd
aouyang-fhsu <78335523+aouyang-fhsu@users.noreply.github.com>
2021-03-15 18:29:32 -0400

    updated connection

--- model/database.php
+++ model/database.php
@@ -1,9 +1,10 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username= 'root';
+    $dsn = 'mysql:host=lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=aouyang-fhsu-inf653midterm-pub';
+    $username= 'oqotc5iofcpe1kde'
+    $pw='hkbstzyjy4ttspkh'    ;
 
     try {
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $pw);
     } catch(PDOException $e) {
         $error_message = 'Database Error: ';
         $error_message .= $e->getMessage();
