fb9ea8ebd049c21133dfad229a969974e232424d
Aaron Ouyang <a_ouyang@mail.fhsu.edu>
2021-03-15 18:35:13 -0400

    conn fix

--- model/database.php
+++ model/database.php
@@ -1,6 +1,6 @@
 <?php
     $dsn = 'mysql:host=lyn7gfxo996yjjco.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=aouyang-fhsu-inf653midterm-pub';
-    $username= 'oqotc5iofcpe1kde'
+    $username= 'oqotc5iofcpe1kde';
     $pw='hkbstzyjy4ttspkh'    ;
 
     try {
